1d82133ad8a7db90f7f2ac8f2b83e8f2ae7e70fa
SPHyrule <stphnpino1@aol.com>
2020-06-05 17:16:10 +0100

    Search database feature

--- Pages/Bugs/Index.cshtml.cs
+++ Pages/Bugs/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using BugTrackerv2.Data;
 using BugTrackerv2.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BugTrackerv2.Pages.Bugs
 {
@@ -17,16 +18,39 @@ namespace BugTrackerv2.Pages.Bugs
     {
         private readonly BugTrackerv2.Data.ApplicationDbContext _context;
 
+        //Properties
+        //contains the text users enter in the search text box. SearchString has the [BindProperty] attribute. [BindProperty] binds form values and query strings with the same name as the property. (SupportsGet = true) is required for binding on GET requests.
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public IList<Bug> Bug { get; set; }
+
         public IndexModel(BugTrackerv2.Data.ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public IList<Bug> Bug { get; set; }
-
         public async Task OnGetAsync()
         {
             Bug = await _context.BugForms.ToListAsync();
+        }
+
+        public async Task OnGetAsyncFilter()
+        {
+            // Use LINQ to get list of bugs.         
+
+            var bugs = from m in _context.BugForms
+                       select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                //A Lambda Expression. 
+                bugs = bugs.Where(s => s.BugType.Contains(SearchString));
+            }
+
+
+            //Bug = await bugs.ToListAsync();
+            Bug = await bugs.ToListAsync();
         }
     }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BugTrackerv2.Data;
+using BugTrackerv2.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace BugTrackerv2.Pages
@@ -14,18 +18,57 @@ namespace BugTrackerv2.Pages
     {
         //Fields
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
+
+        //Properties
+        //contains the text users enter in the search text box. SearchString has the [BindProperty] attribute. [BindProperty] binds form values and query strings with the same name as the property. (SupportsGet = true) is required for binding on GET requests.
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Bugs { get; set; }
+
+        //contains the specific bugtype the user selects (for example, "Website").
+        [BindProperty(SupportsGet = true)]
+        public string BugType { get; set; }
+
+        public IList<Bug> Bug { get; set; }
 
         //Constructor - injected IConfiguration resolve the user credentials from appSettings.
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             this._logger = logger;
+            this._context = context;
 
         }
 
-        //Http Get request Method
-        public void OnGet()
+        //Http Get request Method  
+        public IActionResult OnGet()
         {
+            return Page();
+        }
 
+
+        public async Task<IActionResult> OnGetRead()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var bugs = from b in _context.BugForms
+                       select b;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                //Use of a Lambda Expression. 
+                bugs = bugs.Where(s => s.BugType.Contains(SearchString));
+            }
+
+            Bug = (IList<Bug>)await bugs.ToListAsync();
+
+            //return Page();
+            return new RedirectToPageResult("/Bugs/Index");
         }
 
 
