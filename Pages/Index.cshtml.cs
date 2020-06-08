3428e03dff5e9b0093946888f00cd8776e665053
SPHyrule <stphnpino1@aol.com>
2020-06-08 13:36:39 +0100

    css section added

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -21,18 +21,15 @@ namespace BugTrackerv2.Pages
         private readonly ApplicationDbContext _context;
 
         //Properties
-        //contains the text users enter in the search text box. SearchString has the [BindProperty] attribute. [BindProperty] binds form values and query strings with the same name as the property. (SupportsGet = true) is required for binding on GET requests.
+        //SearchString contains the text users enter in the search text box. SearchString has the [BindProperty] attribute. [BindProperty] binds form values and query strings with the same name as the property. (SupportsGet = true) is required for binding on GET requests.
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
-
-        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
-        public SelectList Bugs { get; set; }
 
         //contains the specific bugtype the user selects (for example, "Website").
         [BindProperty(SupportsGet = true)]
         public string BugType { get; set; }
 
-        public IList<Bug> Bug { get; set; }
+        public IList<Bug> Bugs { get; set; }
 
         //Constructor - injected IConfiguration resolve the user credentials from appSettings.
         public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
@@ -42,34 +39,28 @@ namespace BugTrackerv2.Pages
 
         }
 
-        //Http Get request Method  
-        public IActionResult OnGet()
+        //Http Get request Method - The first line of the OnGetAsync method creates a LINQ query to select the bugs:
+        public async Task<PageResult> OnGetAsync()
         {
-            return Page();
-        }
-
-
-        public async Task<IActionResult> OnGetRead()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
+            // using System.Linq;
             var bugs = from b in _context.BugForms
                        select b;
 
+            //If the SearchString property is not null or empty, the bugs query is modified to filter on the search string:
             if (!string.IsNullOrEmpty(SearchString))
             {
                 //Use of a Lambda Expression. 
-                bugs = bugs.Where(s => s.BugType.Contains(SearchString));
+                bugs = bugs.Where(s => s.Title.Contains(SearchString));
             }
 
-            Bug = (IList<Bug>)await bugs.ToListAsync();
+            Bugs = await bugs.ToListAsync();
 
-            //return Page();
-            return new RedirectToPageResult("/Bugs/Index", Bug);
+            return Page();
+
         }
+
+
+
 
 
     }
